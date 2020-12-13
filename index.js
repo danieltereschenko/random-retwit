2f89e0074013a2645edabddd1e5072837d3bb286
Danila <danila@datainlife.ru>
2020-12-14 00:20:35 +0300

    added webpack

--- index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SomeComponent = () => {
-  return (
-    <div>31231</div>
-  );
-}
-
-export default SomeComponent;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SomeComponent = () => (
+  321312
+);
+
+export default SomeComponent;
